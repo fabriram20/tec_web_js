943f78a4932403ea0e1b355aefbffc0026d3f971
Fabri <fabriram20@gmail.com>
2016-11-23 14:40:31 -0500

    Generar nueva version del paquete + instalar la nueva version

--- NodeJS2/node_modules/tecnologiaswebrfabrizio/app.js
+++ NodeJS2/node_modules/tecnologiaswebrfabrizio/app.js
@@ -1,5 +1,16 @@
+//tecnologiaswebrfabrizio: imprimirSuma1y2();
+//tecnologiaswebrfabrizio: imprimirSuma1y2();
+
+module.exports={
+    imprimirSuma1y2: imprimirSuma1y2, 
+}
+
+function imprimirSuma1y2(){
 var numero1 = 1,
     numero2 = 2;
-
-
+//se coore en la consola node
+//con el comando  >node app.js
 console.log(numero1 + numero2);
+}
+
+
