e2dd23338e1ee0c5de603355a8531bc92577d0b5
fabriram20 <fabriram20@gmail.com>
2016-11-30 13:47:06 -0500

    Refactor app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var puerto=5050;
 
 app.get('/', function (req, res) {
   res.send('Hello World! con java script')
@@ -9,6 +10,6 @@ app.post('/', function (req, res) {
   res.send('POSTTT HEREEEEEE!!!!!!!!!!')
 })
 
-app.listen(5050, function () {
-  console.log('Example app listening on port 5050!')
+app.listen(puerto, function () {
+  console.log('Example app listening on port '+puerto+'!')
 })
