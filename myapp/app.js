31e177637da8d417d41e10375ac0aacbe858e88e
fabriram20 <fabriram20@gmail.com>
2016-11-30 13:55:32 -0500

    request y response

--- myapp/app.js
+++ myapp/app.js
@@ -2,15 +2,24 @@ var express = require('express')
 var app = express()
 var puerto=5050;
 
-app.get('/', function (req, res) {
+app.get('/tec_web_js', function (req, res) {
   res.send('Hello World! con java script')
 })
 
-app.post('/', function (req, res) {
+app.post('/tec_web_js', function (req, res) {
+    
+    //req => request
+    
+    //res => response
+    console.log('Lo que tengo en el request ');    
+    console.log(req.headers);
+    console.log('Lo que tengo en el response ');
+    console.log(res.headers);
+    
   res.send('POSTTT HEREEEEEE!!!!!!!!!!')
 })
 
-app.put('/', function (req, res) {
+app.put('/tec_web_js', function (req, res) {
   res.send('PuuuuuTTT HEREEEEEE!!!!!!!!!!')
 })
 
