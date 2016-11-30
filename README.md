26da472c5091a5ecb00a668ac0b81e17cf563496
fabriram20 <fabriram20@gmail.com>
2016-11-30 14:36:06 -0500

    creacion d enuevo metodo get + envio de aprametros

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SEGUNDA PARTE CLASE NODE JS
 
+# fecha 30/11/2016
+
 
 En esta clase instalaremos un servidor http dinamico con express para esto nos dirigimos al siguietne enlace y seguimos los pasos:
 
@@ -16,11 +18,26 @@ se instala npm con
 
 
 
-luego de terminar el tutorial instalacion y hola mundo 
+* luego de terminar el tutorial instalacion y hola mundo 
 
-agregamos respuestas para el metodo post y put
+* agregamos respuestas para el metodo post y put
 
-Verificamos que los metodos funcionan para ello utilizamos postman para enviar peticiones tanto post como put, se despliega lo que se programo
+* Verificamos que los metodos funcionan para ello utilizamos postman para enviar peticiones tanto post como put, se despliega lo que se programo
 
 
-luego agregamos codigo en el metodo post para poder visualizar por consola la informacion de las cabeceras
+* luego agregamos codigo en el metodo post para poder visualizar por consola la informacion de las cabeceras
+
+* intentamos poner una segunda respuesta, el navegador nos dice q es imposible, nos da un error probar:
+
+
+* ahora cambios las respuesta que enviamos, usaremos json para esto
+
+sen.json(usuario)
+
+
+* Como se observo antes las cabeceras esta indefinidas
+
+
+* ahora vamos agergar una nueva propiedad ala cabecera del metodo post y revisamos que funciono con postman
+
+
--- myapp/app.js
+++ myapp/app.js
@@ -1,26 +1,95 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var puerto=5050;
 
-app.get('/tec_web_js', function (req, res) {
-  res.send('Hello World! con java script')
+//estos son nuestros usuario que se  utilizaran para la entraga de parametros
+var usuarios = [
+    {
+        id:1,
+        nombre:'Pepe',
+        cedula:'123409182'
+    },
+    {
+        id:2,
+        nombre:'Carlos',
+        cedula:'981237918'
+    },
+    {
+        id:3,
+        nombre:'Juan',
+        cedula:'011283934'
+    }
+]
+
+
+var usuario = {
+    "nombre": 'fabri',
+    "cedula": '0000000000'
+}
+
+//esta es una forma de agregar mas valores a un objeto 
+//usuario={
+//    nombre: usuario.nombre,
+//    cedula: usuario.cedula,
+//    apellido: 'holo'    
+//}
+
+
+//otra forma de agregar mas valores en un objeto
+
+usuario.apellido = '';
+usuario.mascotas = [];
+usuario.casado = false;
+//
+////Get normal
+//app.get('/tec_web_js', function (req, res) {
+//  res.send('Hello World! con java script');
+//})
+
+//Get con parametros
+app.get('/Usuario/:idUsuario', function (req, res) {
+    
+    var idActual = req.params.idUsuario;
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario');
+    
 })
 
 app.post('/tec_web_js', function (req, res) {
     
+        
     //req => request
     
     //res => response
-    console.log('Lo que tengo en el request ');    
-    console.log(req.headers);
-    console.log('Lo que tengo en el response ');
+//    console.log('Lo que tengo en el request ');
+//    //agregamos una nueva propiedad ala cabecera
+//    //req.headers.token = 1234; //no funciona
+//        
+//    console.log(req.headers);
+//    console.log('Lo que tengo en el response ');
+//    
+    res.append('toke', '1234');
     console.log(res.headers);
     
-  res.send('POSTTT HEREEEEEE!!!!!!!!!!')
+  //res.send('POSTTT HEREEEEEE!!!!!!!!!!')
+  // segunda respuesta: esta causa un error pues no es valido enviar
+  //res.send('POSTTT HEREEEEEE!!!!!!!!!!')
+    
+    //ahora enviaremos de respuesta objetos .json
+    res.json(usuario);
+  
 })
 
+
+
 app.put('/tec_web_js', function (req, res) {
-  res.send('PuuuuuTTT HEREEEEEE!!!!!!!!!!')
+  
+res.send('PuuuuuTTT HEREEEEEE!!!!!!!!!!')
+    
 })
 
 app.listen(puerto, function () {
