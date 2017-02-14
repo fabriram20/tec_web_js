92b058cdebf9697fa89cb06eb4b1bfaa3808fd08
4 <8>
2017-02-14 14:39:53 -0500

    machine packs password

--- Mascotas/api/models/Usuario.js
+++ Mascotas/api/models/Usuario.js
@@ -5,39 +5,55 @@
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
+var Passwords = require('machinepack-passwords');
+
 module.exports = {
 
-  attributes: {
-      nombres:{
-          type:'string',
-          minLength:5,
-          required:true
-      },
-      apellidos:{
-          type:'string',
-          minLength:5,
-          required:true
-      },
-      correo:{
-          type:'string',
-          email:true,
-          defaultsTo:'correo@invalido.com'
-      },
-      passwor:{
-          type:'string',
-          defaultsTo:"123456"
-      }
-  }
-    
+    attributes: {
+        nombres: {
+            type: 'string',
+            minLength: 5,
+            required: true
+        },
+        apellidos: {
+            type: 'string',
+            minLength: 5,
+            required: true
+        },
+        correo: {
+            type: 'string',
+            email: true,
+            defaultsTo: 'correo@invalido.com'
+        },
+        password: {
+            type: 'string',
+            defaultsTo: "123456"
+        }
+    },
+
+    beforeCreate: function (values, cb) {
+
+        sails.log.info(values);
+        //        cb(132135232)//Error
+//        cb() //ok si no colocamos el callbak entonces el servidor nuca devuelve los datos
+            //        cb("hola");
+        Passwords.encryptPassword({
+            password: values.password,
+        }).exec({
+            // An unexpected error occurred.
+            error: function (err) {
+                cb(err)
+
+            },
+            // OK.
+            success: function (result) {
+                values.password = result;
+                cb();
+            },
+        });
+
+
+
+    }
+
 };
-
-
-
-
-
-
-
-
-
-
-
