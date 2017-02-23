92c2745c0b640626675de96d871c9142c1f5b539
fabriram20@gmail.com <fabriram20@gmail.com>
2017-02-23 18:05:10 -0500

    iniciar formulario

--- AplicacionPrueba/FrontEnd/proj/src/app/app.component.ts
+++ AplicacionPrueba/FrontEnd/proj/src/app/app.component.ts
@@ -1,24 +1,43 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-  apellido="hola";
+export class AppComponent implements OnInit{
+  apellido="";
+  nombre:string="";
   title:string = "Hola amigos";
-  constructor() {
-    this.apellido = this.saludar();
-    this.saludar2("Ram");
-  }
-  saludar(): string {
-    console.log("Hola");
-    return `Hi everyone, mi nombre es: ${this.apellido}`;
-  }
-  saludar2(apellido:string, nombre?:string){
-    console.log(`${apellido} ${nombre}`);
-  }
+  colorH4 = "red";
+  tamanoH4 = "25px";
+  classes = "button";
+  nuevaTienda:any={};
 
+  constructor() {
+    this.nombre="ram";
+    this.apellido="fabro";
+    console.log("inicio el contructor")
+  }
+  ngOnInit(){
+    this.apellido="Cutimbo";
+    this.nombre="Eloy";
+    console.log("On Init")
+  }
+  nombreCompleto():string{
+    return `${this.nombre} ${this.apellido}`
+  }
+  hizoClick(){
+    console.log("Hizo Click");
+
+  }
+  hizoFocus(){
+    console.log("Hizo Focus");
+  }
+  backgroudColor(){
+    return "red";
+  }
+  crearTienda(formulario){
+    console.log(formulario);
+  }
 }
